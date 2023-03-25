726e91ed80669cb0ad82797b10f9e0b5ec800bd2
Iroda Yılmaz <irodaand@hotmail.com>
2023-03-25 03:58:31 +0300

    final

--- index.js
+++ index.js
@@ -11,6 +11,22 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+const titleCased = () =>{
+  return tutorials.map(tutorial => title(tutorial));
+}
+
+function title(array){
+    let stringArray = array.split(" ");
+    let arr1 = stringArray.map(str => {
+      let newStr = "";
+      if(str.charAt(0) <= 'z' && str.charAt(0) >= 'a'){
+        let firstChar = str.charAt(0).toUpperCase();
+        newStr = firstChar+ str.slice(1);
+        return newStr;
+      }
+      return str;
+      
+    });
+    return arr1.join(" ");
+  
 }
